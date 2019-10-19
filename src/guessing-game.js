f1c4843295537364574ae03bf4c0486cbe679082
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2019-10-19 22:56:00 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.mid;
     }
 
     guess() {
-
+        this.mid = Math.round((this.min + this.max)/2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
